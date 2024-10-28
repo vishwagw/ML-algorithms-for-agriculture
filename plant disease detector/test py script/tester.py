ac5e13360fc3efa039f77a4a0ddf43ed28fa65d5
Vishwa GW <136871744+vishwagw@users.noreply.github.com>
2024-10-29 02:58:45 +0530

    Add files via upload

---
+++ plant disease detector/test py script/tester.py
@@ -0,0 +1,18 @@
+#library 
+import os
+
+folders = os.listdir()
+
+for i in folders:
+    if i == 'tester.py':
+        pass
+    else:
+        folder_path = os.path.join(os.getcwd(), i)
+        file_names = os.listdir(folder_path)
+        name = 1
+        for file in file_names:
+            # path = os.path.join(folder_path,file)
+            os.rename(os.path.join(folder_path,file),os.path.join(folder_path, str(name) + '.jpg'))
+            print(type(name))
+            name = name + 1
+
